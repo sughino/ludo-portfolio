4edbcb59845f0ad083f270bd169caf292e1149ca
sughino <ludograsso08@gmail.com>
2026-02-14 15:39:40 +0100

    lifestyle adjustement

--- src/app/data/hobbies.ts
+++ src/app/data/hobbies.ts
@@ -1,8 +1,32 @@
 import type { CarouselType } from '@/app/types/carouselType';
 
 export const hobbies: CarouselType[] = [
-    {title: 'capturing moments', description: '', img: '', width: 1, height: 1},
-    {title: 'feeling rhythms', description: '', img: '', width: 1, height: 1},
-    {title: 'exploring', description: '', img: '', width: 1, height: 1},
-    {title: 'street culture', description: '', img: '', width: 1, height: 1}
+    {
+        title: 'capturing moments', 
+        description: 'For me, photography is about freezing time. I like to observe the light and capture the spontaneity of my surroundings.', 
+        img: '/lifestyle/1.webp', 
+        width: 600, 
+        height: 400
+    },
+    {
+        title: 'feeling rhythms', 
+        description: `Music is what drives my day. Whether it's to energize me while I work or to help me relax, there's never a moment of silence.`, 
+        img: '/lifestyle/2.webp', 
+        width: 600, 
+        height: 400
+    },
+    {
+        title: 'exploring', 
+        description: 'Traveling means stepping outside your comfort zone. I love discovering new places, different cultures, and getting lost in unfamiliar streets.', 
+        img: '/lifestyle/3.webp', 
+        width: 600, 
+        height: 400
+    },
+    {
+        title: 'street culture', 
+        description: `From skateboarding to graffiti, I love the energy of the underground world. It's all about style, freedom, and unfiltered creativity.`, 
+        img: '/lifestyle/4.webp', 
+        width: 600, 
+        height: 400
+    }
 ]
--- src/app/ui/page/lifeStyle/index.tsx
+++ src/app/ui/page/lifeStyle/index.tsx
@@ -1,48 +1,73 @@
+'use client'
+
+import Image from 'next/image';
 import Carousel from '../../components/carousel'
 import styles from './lifeStyle.module.css'
-import { hobbies } from '@/app/data/hobbies';
+import { hobbies } from '@/app/data/hobbies'
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
 export default function LifeStyle () {
+    const imgHolderRef = useRef<(HTMLDivElement | null)[]>([]);
+    const contentRef = useRef<(HTMLDivElement | null)[]>([]);
+
+    // useEffect(() => {
+    //     imgHolderRef.current.forEach((img, i) => {
+    //         const content = contentRef.current[i]
+    //         if (!img || !content) return
+
+    //         const tl = gsap.timeline({ paused: true })
+
+    //         tl.to(content, {
+    //             opacity: 0,
+    //             x: -20,
+    //             // width: 0,
+    //             duration: 0.4,
+    //             ease: 'power2.out'
+    //         }).to(img, {
+    //             duration: 0.4,
+    //             ease: 'power2.out'
+    //         }, '<')
+
+    //         img.addEventListener('mouseenter', () => tl.play())
+    //         img.addEventListener('mouseleave', () => tl.reverse())
+    //     })
+    // }, [])
+
     return (
-        <section className="bigSpacing">
+        <section className="noSpacing">
             <div className="h-(--spacing-160)" />
             <h2>LiFeSTYlE</h2>
             <div className="h-(--spacing-80)" />
 
             <div className={`hidden md:grid ${styles.lifeStyleContainer}`}>
-                <div className={styles.lifeStyleInnerContainer}>
-                    <div className={styles.holderImg}></div>
-                    <div className={styles.lifeStyleContentContainer}>
-                        <h3>capturing moments</h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
+                {hobbies.map((hobby, i) => (
+                    <div key={i} className={styles.lifeStyleInnerContainer} data-variant={i%2 !== 0 ? 'reverse' : '' }>
+                        <div 
+                            className={styles.holderImg}
+                            ref={(el) => { imgHolderRef.current[i] = el }}
+                        >
+                            <Image 
+                                src={hobby.img}
+                                width={hobby.width}
+                                height={hobby.height}
+                                alt={`${hobby.title} img`}
+                            />
+                        </div>
+                        <div 
+                            className={styles.lifeStyleContentContainer}
+                            ref={(el) => { contentRef.current[i] = el }}
+                        >
+                            <h3>{hobby.title}</h3>
+                            <p>{hobby.description}</p>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.lifeStyleInnerContainer} data-variant="reverse">
-                    <div className={styles.holderImg}></div>
-                    <div className={styles.lifeStyleContentContainer}>
-                        <h3>feeling rhythms</h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
-                    </div>
-                </div>
-                <div className={styles.lifeStyleInnerContainer}>
-                    <div className={styles.holderImg}></div>
-                    <div className={styles.lifeStyleContentContainer}>
-                        <h3>exploring</h3>
-                        <p>Lorem Ipsum is simply. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
-                    </div>
-                </div>
-                <div className={styles.lifeStyleInnerContainer} data-variant="reverse">
-                    <div className={styles.holderImg}></div>
-                    <div className={styles.lifeStyleContentContainer}>
-                        <h3>street culture</h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
-                    </div>
-                </div>
+                ))}
             </div>
 
-            {/* <div className={styles.carouselContainer}>
+            <div className={styles.carouselContainer}>
                 <Carousel data={hobbies} />
-            </div> */}
+            </div>
 
             <div className="h-(--spacing-160)" />
         </section>
