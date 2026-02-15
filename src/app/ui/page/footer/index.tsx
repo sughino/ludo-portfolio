c34d40eaf9936f5b49bf2675009cf34deda55830
sughino <ludograsso08@gmail.com>
2026-02-16 00:43:27 +0100

    adjusted footer

--- src/app/ui/page/footer/index.tsx
+++ src/app/ui/page/footer/index.tsx
@@ -6,33 +6,37 @@ export default function Footer () {
     return (
         <footer>
             <div className="h-(--spacing-padding-50)" />
-            <div className={styles.footerTitleContainer}>
-                <LogoAnimation variant='footer'/> 
-                <h3>web developer <span className={styles.coloredSpan}>&</span><br/>designer</h3>
-            </div>
-            <div className={styles.contactContainer}>
-                <div className={styles.contact}>
-                    <p className={styles.contactTitle}>Phone number</p>
-                    <p className={styles.contactContent}>+39 333 955 3916</p>
+            
+            <div className={styles.footerContainer}>
+                <div className={styles.footerTitleContainer}>
+                    <LogoAnimation variant='footer'/> 
+                    <h3>web developer <span className={styles.coloredSpan}>&</span><br/>designer</h3>
                 </div>
-                <div className={styles.contact}>
-                    <p className={styles.contactTitle}>Email</p>
-                    <p className={styles.contactContent}>ludograsso08@gmail.com</p>
-                </div>
-                <div className={styles.contact}>
-                    <p className={styles.contactTitle}>Quick links</p>
-                    <div className={styles.contactContainer}>
-                        <div className={styles.linksContainer}>
-                            <p className={styles.contactContent}>Github</p>
-                            <ExternalLink />
-                        </div>
-                        <div className={styles.linksContainer}>
-                            <p className={styles.contactContent}>Linkedin</p>
-                            <ExternalLink />
+                <div className={styles.contactContainer}>
+                    <div className={styles.contact}>
+                        <p className={styles.contactTitle}>Phone number</p>
+                        <p className={styles.contactContent}>+39 333 955 3916</p>
+                    </div>
+                    <div className={styles.contact}>
+                        <p className={styles.contactTitle}>Email</p>
+                        <p className={styles.contactContent}>ludograsso08@gmail.com</p>
+                    </div>
+                    <div className={styles.contact}>
+                        <p className={styles.contactTitle}>Quick links</p>
+                        <div className={styles.contactContainer}>
+                            <div className={styles.linksContainer}>
+                                <p className={styles.contactContent}>Github</p>
+                                <ExternalLink />
+                            </div>
+                            <div className={styles.linksContainer}>
+                                <p className={styles.contactContent}>Linkedin</p>
+                                <ExternalLink />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div className="h-(--spacing-160)" />
         </footer>
     )
