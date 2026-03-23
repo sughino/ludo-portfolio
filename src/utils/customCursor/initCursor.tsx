a29edb1a83e0c7d299ea5a87fad2d3bbfb7f87f6
sughino <ludograsso08@gmail.com>
2026-03-23 18:49:14 +0100

    fixed cursor error

--- src/utils/customCursor/initCursor.tsx
+++ src/utils/customCursor/initCursor.tsx
@@ -3,15 +3,13 @@
 import { useEffect } from 'react'
 import { useIsTouch } from '@/contexts/DeviceContext'
 import { CustomCursor } from './customCursor'
-import { hobbies } from '@/data/hobbies'
  
 export default function CursorInit() {
   const isTouch = useIsTouch();
-  const images = hobbies.map(i => i.img)
  
   useEffect(() => {
     if (isTouch) return;
-    const cursor = CustomCursor(images);
+    const cursor = CustomCursor();
     return () => cursor.destroy();
   }, [isTouch]);
  
