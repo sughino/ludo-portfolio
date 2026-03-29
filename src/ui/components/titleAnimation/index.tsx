ab05e11083157f16fd62c93afff3ab2ebefa1d3c
Ludovico <ludograsso08@gmail.com>
2026-03-30 00:08:25 +0200

    fixed animation title

--- src/ui/components/titleAnimation/index.tsx
+++ src/ui/components/titleAnimation/index.tsx
@@ -1,58 +1,50 @@
-'use client'
+  'use client'
 
-import { useEffect, useRef, ReactNode } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import styles from './titleAnimation.module.css';
+  import { useEffect, useRef, ReactNode } from "react";
+  import gsap from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+  import styles from './titleAnimation.module.css';
 
-gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger);
 
-type Props = {
-  children: ReactNode;
-  delay?: number;
-  position?: string;
-  reverse?: boolean;
-  duration?: number;
-}
+  type Props = {
+    children: ReactNode;
+    delay?: number;
+    position?: string;
+    reverse?: boolean;
+    duration?: number;
+  }
 
-export default function TitleAnimation({ 
-    children, 
-    delay = 0,
-    position,
-    reverse = false,
-    duration = .8
-}: Props) {
-  const titleContainerRef = useRef<HTMLDivElement>(null);
+  export default function TitleAnimation({ 
+      children, 
+      delay = 0,
+      position,
+      reverse = false,
+      duration = .8
+  }: Props) {
+    const titleContainerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const titles = titleContainerRef.current?.querySelectorAll('[data-animate="title"]');
-    if (!titles) return;
+    useEffect(() => {
+      const titles = titleContainerRef.current?.querySelectorAll('[data-animate="title"]');
+      if (!titles) return;
 
-    gsap.fromTo(
-      titles,
-      { 
-        bottom: reverse ? 0 : -180,
-        filter: reverse ? "blur(0px)" : "blur(10px)"
-      },
-      {
+      gsap.to(titles, {
         bottom: reverse ? -180 : 0,
         filter: reverse ? "blur(10px)" : "blur(0px)",
         duration: duration,
         ease: "power4.out",
-        force3D: true,
         delay: delay,
         scrollTrigger: {
           trigger: titleContainerRef.current,
           start: "top 100%",
           once: true
         }
-      }
-    );
-  }, [delay, reverse]);
+      });
+    }, [delay, reverse]);
 
-  return (
-    <div className={styles.titleContainer} data-position={position} ref={titleContainerRef}>
-      {children}
-    </div>
-  );
-}
+    return (
+      <div className={styles.titleContainer} data-position={position} ref={titleContainerRef}>
+        {children}
+      </div>
+    );
+  }
